56fc30ee8952fe77478cc8e9db838f2fb31833eb
Anthony Parrino <anthony21parrino21@gmail.com>
2021-05-29 14:40:30 -0400

    added shirt images

--- client/src/Components/HomeComp/HomeComp.js
+++ client/src/Components/HomeComp/HomeComp.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import "./homeComp.css"
 import Header from "../Header/Header";
-import RaceBack from "./race-back.png";
+import RaceBlack from "./alphawave1.jpg";
+import RaceWhite from "./alphawave2.jpg";
+import TwoEyesBlack from "./alphawave4.jpg";
+import TwoEyesWhite from "./alphawave5.jpg";
+import Pyrimad from "./alphawave7.jpg";
+import EyeCheck from "./alphawave8.jpg";
+// import Palm from "./alphawave9.jpg";
 
 function HomeComp() {
     return (
         <div>
             <Header />
-            <div className="underHeaderText">
-                <p>first release now available</p>
-                <img className="raceBack" src={RaceBack} alt="raceBlack"></img>
+            <div>
+                <div className="underHeaderText">
+                    <p>first release now available</p>
+                </div>
+                <div className="shirtDivs">
+                    <img className="raceFront" src={RaceBlack} alt="raceBlack"></img>
+                    <img className="raceFront" src={RaceWhite} alt="raceFront"></img>
+                </div>
+                <div className="shirtDivs">
+                    <img className="raceFront" src={TwoEyesBlack} alt="raceBlack"></img>
+                    <img className="raceFront" src={TwoEyesWhite} alt="raceFront"></img>
+                </div>
+                <div className="shirtDivs">
+                    <img className="raceFront" src={EyeCheck} alt="raceFront"></img>
+                    <img className="raceFront" src={Pyrimad} alt="raceBlack"></img>
+                </div>
+                {/* <div className="shirtDivs">
+                    <img className="raceFront" src={Palm} alt="raceFront"></img>
+                </div> */}
             </div>
         </div>
     )
